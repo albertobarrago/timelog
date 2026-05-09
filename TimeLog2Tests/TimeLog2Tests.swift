1dca772d4f6406491ecb705e55c920fe04b3d04b
Alberto Barrago <albertobarrago@gmail.com>
2026-05-10 00:16:51 +0200

    Initial Commit

---
+++ TimeLog2Tests/TimeLog2Tests.swift
@@ -0,0 +1,19 @@
+//
+//  TimeLog2Tests.swift
+//  TimeLog2Tests
+//
+//  Created by Alberto Barrago on 10/05/2026.
+//
+
+import Testing
+@testable import TimeLog2
+
+struct TimeLog2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
